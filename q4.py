bad1e50729e12346d89206727eb1b2b9294df303
Hummingliberty616 <wlin2028@kehillahstudent.org>
2026-04-24 11:18:52 -0700

    -

--- q4.py
+++ q4.py
@@ -4,7 +4,8 @@ import json
 # Check if the file "data.json" exists.
 if os.path.isfile("data.json"):
     # If it does, read "data.json" into the variable `data`
-
+    with open("data.json", "r") as f:
+        data = f.read() 
     ####
     #### YOUR CODE HERE 
     ####
@@ -19,6 +20,9 @@ recommendation = input("What book/movie/podcast/etc. would you recommend? ")
 
 # Add the new user and recommendation to the `data` dictionary
 data[name] = recommendation
+with open("data.json", "w") as f:
+    f.write(data)
+
 
 # Write the `data` variable to the file "data.json"
 
